82107b023ac4111ed2c1a58c1a3f48c7d91ddf2d
Adarsh Kumar <contactadarshk@gmail.com>
2022-10-29 00:17:33 +0530

    add method to update user password

--- src/user_service/repo/queries.rs
+++ src/user_service/repo/queries.rs
@@ -1,3 +1,4 @@
+use std::future::Future;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use uuid::Uuid;
 use crate::user_service::repo::models;
@@ -23,9 +24,12 @@ pub async fn new(url: String, max_connections: u32) -> Result<RepoService, sqlx:
 impl RepoService {
     pub async fn fetch_user(self: &Self, username: String) -> Result<Option<models::User>, sqlx::Error> {
         let query = r#"
-        SELECT * 
-        FROM users
-        WHERE users.username = $1
+        SELECT 
+            * 
+        FROM
+            users
+        WHERE
+            users.username = $1
         "#;
 
         sqlx::query_as::<_, models::User>(query)
@@ -38,9 +42,12 @@ impl RepoService {
         let timestamp = chrono::Utc::now();
         
         let query = r#"
-        INSERT INTO users(id, created_at, updated_at, username, password)
-        VALUES ($1, $2, $3, $4, $5)
-        RETURNING id, created_at, updated_at, username, password
+        INSERT INTO 
+            users(id, created_at, updated_at, username, password)
+        VALUES 
+            ($1, $2, $3, $4, $5)
+        RETURNING
+            id, created_at, updated_at, username, password
         "#;
     
         sqlx::query_as::<_, models::User>(query)
@@ -52,4 +59,32 @@ impl RepoService {
         .fetch_one(&self.pool)
         .await
     }
+
+    pub async fn update_user_password(
+        self: &Self, 
+        username: String, 
+        password: String
+    ) -> Result<models::User, sqlx::Error> {
+        let timestamp = chrono::Utc::now();
+        
+        let query = r#"
+        UPDATE
+            users
+        SET
+            password = $1
+            updated_at = $2
+        WHERE
+            username = $3
+        RETURNING
+            id, created_at, updated_at, username, password
+        "#;
+    
+        sqlx::query_as::<_, models::User>(query)
+        .bind(password)
+        .bind(timestamp)
+        .bind(username)
+        .fetch_one(&self.pool)
+        .await
+    }
+
 }
