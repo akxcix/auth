6b107f8b711e63c1f86103d923898998981b5e9f
Adarsh Kumar <contactadarshk@gmail.com>
2022-11-02 21:24:30 +0530

    update response struct

--- src/api/models/responses/server_response.rs
+++ src/api/models/responses/server_response.rs
@@ -5,34 +5,45 @@ use axum::{
     response::IntoResponse,
 };
 
+#[derive(Serialize)]
+pub struct Body<T: Serialize> {
+    data: Option<T>,
+    error: Option<String>
+}
+
 pub struct Response<T: Serialize> {
     headers: Vec<(String, String)>,
     status_code: StatusCode,
-    data: Option<T>,
-    error: Option<String>
+    body: Body<T>,
 }
 
 impl<T: Serialize> Response<T> {
     pub fn ok(data: T) -> Response<T> {
         let status_code = StatusCode::OK;
         let headers = vec![];
+        let body = Body {
+            data: Some(data),
+            error: None,
+        };
 
         Response {
             headers: headers,
             status_code: status_code,
-            data: Some(data),
-            error: None
+            body: body,
         }
     }
     
     pub fn error(status_code: StatusCode, message: String) -> Response<T> {
         let headers = vec![];
+        let body = Body {
+            data: None,
+            error: Some(message),
+        };
 
         Response { 
             headers: headers,
             status_code:  status_code,
-            data: None,
-            error: Some(message)
+            body: body,
         }
     }
 }
@@ -40,7 +51,7 @@ impl<T: Serialize> Response<T> {
 impl<T: Serialize> IntoResponse for Response<T> {
     fn into_response(self) -> axum::response::Response {
         let status_code = self.status_code;
-        let json = Json(self.data);
+        let json = Json(self.body);
 
         (status_code, json).into_response()
     }
